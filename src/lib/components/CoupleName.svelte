1ed370a80a4566d5cb029131974015dc4b9261c4
Redshore <isutare412@gmail.com>
2024-12-16 17:12:33 +0900

    feat: add CoupleName component

---
+++ src/lib/components/CoupleName.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	const { brideName, groomName }: { brideName: string; groomName: string } = $props();
+</script>
+
+<div class="p-3">
+	<p class="text-center font-serif text-3xl font-semibold tracking-widest md:text-4xl">
+		{`${groomName} | ${brideName}`}
+	</p>
+</div>
