1fe28b150b530ead2e18edc451c62b4809135154
Redshore <isutare412@gmail.com>
2024-12-16 09:22:01 +0900

    chore: add dependency and config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,19 @@
 import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['Arvo', ...defaultTheme.fontFamily.sans],
+				serif: ['Nanum Myeongjo', ...defaultTheme.fontFamily.serif]
+			},
+			lineHeight: {
+				'extra-loose': '2.5'
+			}
+		}
 	},
 
 	plugins: []
