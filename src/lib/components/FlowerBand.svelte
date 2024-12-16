a67b5bf06f4564b606d9a837cf72e1b58969a3a2
Redshore <isutare412@gmail.com>
2024-12-16 17:12:46 +0900

    feat: add FlowerBand component

---
+++ src/lib/components/FlowerBand.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	const flowerImages = [
+		{
+			src: 'https://dmdi76qgv3i4f.cloudfront.net/miscs/flower-1.png'
+		},
+		{
+			src: 'https://dmdi76qgv3i4f.cloudfront.net/miscs/flower-2.png'
+		},
+		{
+			src: 'https://dmdi76qgv3i4f.cloudfront.net/miscs/flower-3.png'
+		},
+		{
+			src: 'https://dmdi76qgv3i4f.cloudfront.net/miscs/flower-4.png'
+		},
+		{
+			src: 'https://dmdi76qgv3i4f.cloudfront.net/miscs/flower-5.png'
+		}
+	];
+</script>
+
+<div class="flex justify-between">
+	{#each flowerImages as image}
+		<img alt="A flower" src={image.src} class="inline-block h-24" />
+	{/each}
+</div>
