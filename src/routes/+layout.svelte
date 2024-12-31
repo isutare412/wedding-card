4701e512969b10f467011f381372b66790754c0c
Redshore <isutare412@gmail.com>
2025-01-01 00:37:22 +0900

    feat: include font as package

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
 	import ToastContainer from '$lib/components/ToastContainer.svelte';
+	import '@fontsource/arvo/400.css';
+	import '@fontsource/arvo/700.css';
+	import '@fontsource/nanum-myeongjo/400.css';
+	import '@fontsource/nanum-myeongjo/700.css';
+	import '@fontsource/nanum-myeongjo/800.css';
 	import '../app.css';
 
 	let { children } = $props();
